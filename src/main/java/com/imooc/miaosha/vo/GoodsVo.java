ad6767e792df7a8aba85c536ab35ad04d95e6392
hll <hll@moresing.com>
2019-01-03 11:23:38 +0800

    add

---
+++ src/main/java/com/imooc/miaosha/vo/GoodsVo.java
@@ -0,0 +1,22 @@
+package com.imooc.miaosha.vo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import com.imooc.miaosha.domain.Goods;
+import lombok.Data;
+
+@Data
+public class GoodsVo extends Goods{
+	private Double miaoshaPrice;
+	private Integer stockCount;
+	private Date startDate;
+	private Date endDate;
+	private String goodName;
+	private String goodTitle;
+	private String goodImg;
+	private String goodDetail;
+	private BigDecimal goodPrice;
+	private Integer goodStock;
+
+}
